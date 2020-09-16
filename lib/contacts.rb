ba2851d195cd09326ec122327d75f04ef80218a5
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-16 00:09:55 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,25 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_icecream_flavors
+        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
+
+# Alternative solution
+
+def remove_strawberry(contacts)
+  # Assign Freddy Mercury's hash to a variable
+  freddy = contacts["Freddy Mercury"]
+
+  # Iterate over only Freddy Mercury's data
+  freddy.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
+  end
+  contacts
