cfabb2f089450b67ebe36d67cf4c5b218feee846
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-16 00:11:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,18 +1,18 @@
 require 'pry'
-
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we",
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  #     email: "jon_snow@thewall.we", 
+  #     favorite_icecream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
   #     name: "Freddy",
   #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #     favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
+def remove_strawberry(contacts)
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
@@ -37,3 +37,4 @@ def remove_strawberry(contacts)
     end
   end
   contacts
+end 
