d66d8b27ce59f81dfb10ef7f7c80ea8775276537
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-16 00:12:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,40 +1,41 @@
 require 'pry'
+
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we",
-  #     favorite_icecream_flavors: ["chocolate", "vanilla"]
+  #     email: "jon_snow@thewall.we", 
+  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
   #     name: "Freddy",
   #     email: "freddy@mercury.com",
-  #     favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-def remove_strawberry(contacts)
-
+  
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_icecream_flavors
+          data.delete_if { |ice_cream| ice_cream == "strawberry"}
+        end
       end
     end
   end
-end
 
-# Alternative solution
+  # Alternative solution
 
-def remove_strawberry(contacts)
-  # Assign Freddy Mercury's hash to a variable
-  freddy = contacts["Freddy Mercury"]
+  def remove_strawberry(contacts)
+    # Assign Freddy Mercury's hash to a variable
+    freddy = contacts["Freddy Mercury"]
 
-  # Iterate over only Freddy Mercury's data
-  freddy.each do |attribute, value|
-    if attribute == :favorite_icecream_flavors
-      value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    # Iterate over only Freddy Mercury's data
+    freddy.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+      end
     end
-  end
-  contacts
-end 
+    contacts
+  end 
+end
